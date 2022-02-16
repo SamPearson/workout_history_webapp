1277a2cf38e2776b4b2f7fb063fc4d094f86b577
Sam Pearson <sam.ed.pearson@gmail.com>
2022-02-15 21:46:13 -0600

    Basic routes/views

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,11 @@ def create_app():
     #todo: FLEX-2 - move this so the key won't be in github
     app.config['SECRET_KEY'] = 'opieupsmadfa4j3rlemfapoq83japefwoewsfjapo4ifafj'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+
     return app
