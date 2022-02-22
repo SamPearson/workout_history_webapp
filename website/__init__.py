397cd4dfed29d29bb41c13aa92a4d580c410b42a
Sam Pearson <sam.ed.pearson@gmail.com>
2022-02-22 17:05:55 -0600

    Creating database; account creation page

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     #todo: FLEX-2 - move this so the key won't be in github
     app.config['SECRET_KEY'] = 'opieupsmadfa4j3rlemfapoq83japefwoewsfjapo4ifafj'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -11,5 +19,16 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_database(app)
 
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        print('Could not locate database, attempting to create')
+        db.create_all(app=app)
+        print('Database created successfully')
+    else:
+        print('Database located')
