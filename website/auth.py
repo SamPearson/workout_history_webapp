70be058e8f17645a8eb053183ef8cdaec415e24c
Sam Pearson <sam.ed.pearson@gmail.com>
2022-02-21 20:03:57 -0600

    Handling POST reqs on signup page, error/success message flashing

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
     return render_template("login.html")
 
@@ -11,6 +11,23 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than three characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First Name must be two or more characters.', category='error')
+        elif password1 != password2:
+            flash('Password and confirmation do not match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be six or more characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+
     return render_template("sign_up.html")
