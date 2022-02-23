6a17229135f17588132229d5d01013c801475ac8
Sam Pearson <sam.ed.pearson@gmail.com>
2022-02-22 18:37:15 -0600

    Login/logout, restricting pages based on login status

--- website/auth.py
+++ website/auth.py
@@ -2,17 +2,36 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user =  User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+            else:
+                flash('Incorrect password, try again.', category='error')
+                return redirect(url_for('views.home'))
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
@@ -22,7 +41,11 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists', category='error')
+
+        elif len(email) < 4:
             flash('Email must be greater than three characters.', category='error')
         elif len(first_name) < 2:
             flash('First Name must be two or more characters.', category='error')
@@ -39,7 +62,8 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', category='success')
+            login_user(user, remember=True)
             return redirect(url_for('views.home'))
 
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
