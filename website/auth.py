e3bfe9dc227586080165c700780dc69e074db112
Sam Pearson <sam.ed.pearson@gmail.com>
2022-03-04 20:19:56 -0600

    fixed login and redirect issue, added pycharm directory to gitignore

--- website/auth.py
+++ website/auth.py
@@ -19,13 +19,13 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
-                return redirect(url_for('views.home'))
         else:
             flash('Email does not exist.', category='error')
 
-    return render_template("home.html", user=current_user)
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
