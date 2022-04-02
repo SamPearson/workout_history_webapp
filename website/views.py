13142900c16ad00deffacd89f9cca00d4f351988
Sam Pearson <sam.ed.pearson@gmail.com>
2022-04-02 16:33:18 -0500

    Proof of concept -  can now add new 'workout phase' to the db

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, WorkoutPhase
 from . import db
 import json
 
@@ -33,3 +33,22 @@ def delete_note():
             db.session.delete(note)
             db.session.commit()
     return jsonify({})
+
+@views.route('/admin', methods=['GET','POST'])
+def admin():
+    if request.method == 'POST':
+        workout_phase = request.form.get('workout_phase_input')
+        print(f'wow you input {workout_phase}')
+        if len(workout_phase) < 1:
+            flash('Workout Phase Name must not be empty.', category='error')
+        else:
+            new_workout_phase = WorkoutPhase(name=workout_phase)
+            db.session.add(new_workout_phase)
+            db.session.commit()
+            flash(f'Workout Phase "{workout_phase}" added!', category='success')
+
+    phases = WorkoutPhase.query.all()
+    return render_template("admin.html", user=current_user, phases=phases)
+
+#@views.route('/exercises', methods=['GET'])
+#    return render_template("exercises.html") #maybe define a user or something
